c1f9569ee6d8ded71c3244bb08031ae95f6f437e
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-09-03 15:18:00 +0530

    page + route setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,11 +3,25 @@ import Nav from "./components/Nav";
 import ContestCard from "./components/ContestCard";
 import { Typography } from "@cred/neopop-web/lib/components";
 import { colorPalette, FontVariant } from "@cred/neopop-web/lib/primitives";
+
+import Home from "./pages/Home";
+import Contests from "./pages/Contests";
+import Contest from "./pages/Contest";
+import Leaderboard from "./pages/Leaderboard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <Nav />
-      
+      <Router>
+        <Routes>
+          <Route path="/contest/:id" element={<Contest />} />
+          <Route path="/leaderboard/:id" element={<Leaderboard />} />
+          <Route path="/contests" element={<Contests />} />
+          <Route path="/" element={<Home />} />{" "}
+        </Routes>
+      </Router>
     </div>
   );
 };
--- client/src/pages/Leaderboard.tsx
+++ client/src/pages/Leaderboard.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useParams } from 'react-router-dom';
 
 function Leaderboard() {
-  return <div>Leaderboard</div>;
+  const { id } = useParams();
+
+  return <div>{id}</div>;
 }
 
 export default Leaderboard;
