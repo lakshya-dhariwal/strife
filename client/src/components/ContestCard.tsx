225e9a8c2a1220650a36e1e63b4e511335f4bc13
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-09-03 18:45:51 +0530

    better ui : cards

--- client/src/components/ContestCard.tsx
+++ client/src/components/ContestCard.tsx
@@ -25,26 +25,36 @@ const ContestCard: React.FC<{
       <div className="flex flex-col w-full h-full  items-center border-t-[1px] border-l-[1px] py-3">
         <Typography
           {...FontVariant.HeadingBold28}
-          fontSize={18}
+          fontSize={20}
           color={colorPalette.neoPaccha[500]}
+          style={{ margin: " 0rem 0rem 1rem 0rem" }}
         >
           {contestName}
         </Typography>
-        <h3 className="text-[16px]">Ends in 12:34:33</h3>
-        <h3>1 $MATIC</h3>
-        <div className="mt-1 flex flex-col ">
+        <div className="">
+          <h3 className="flex items-center  m-1">
+            <img src="/images/clock.svg" className="pr-2" /> 12:22:44 Time Left
+          </h3>
+          <h3 className="flex items-center m-1">
+            <img src="/images/ticket.svg" className="pr-2" /> Enter for 1 $MATIC
+          </h3>
+        </div>
+        <div className="mt-1 flex flex-col w-[90%]">
           <Button
             variant="secondary"
-            kind="flat"
+            kind="elevated"
             size="medium"
             colorMode="light"
             fullWidth
-            style={{ margin: " 0.5rem 0rem" }}
+            style={{ margin: " 0.25rem 0rem" }}
             onClick={() => {
               console.log("I'm clicked");
             }}
           >
-            View LeaderBoard
+            <div className="flex  mx-auto">
+              <img src="/images/leaderboard.svg" className="pr-2" />
+              <> LeaderBoard</>
+            </div>
           </Button>
           <Button
             variant="secondary"
@@ -55,9 +65,11 @@ const ContestCard: React.FC<{
               console.log("I'm clicked");
             }}
             fullWidth
-            style={{ margin: " 0.5rem 0rem" }}
+            style={{ margin: " 0.25rem 0rem 0.1rem 0rem" }}
           >
-            Join Contest
+            <div className="flex  mx-auto">
+              <img src="/images/eth.svg" className="pr-2" /> <>Join Contest</>
+            </div>
           </Button>
         </div>
       </div>
