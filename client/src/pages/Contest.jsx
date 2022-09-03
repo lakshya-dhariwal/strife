612e20bbfce3797a3c580d89e4b188c6297becae
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-09-03 23:08:45 +0530

    feat: submit score

--- client/src/pages/Contest.jsx
+++ client/src/pages/Contest.jsx
@@ -1,10 +1,26 @@
 import React, { useState } from "react";
 import PacmanEmbed from "../components/PacmanEmbed";
-import { ElevatedCard } from "@cred/neopop-web/lib/components";
+import {
+  ElevatedCard,
+  InputField,
+  Button,
+  Typography,
+} from "@cred/neopop-web/lib/components";
+import { colorPalette, FontVariant } from "@cred/neopop-web/lib/primitives";
+import Modal from "../components/Modal";
 
 function Contest() {
   const [score, setScore] = React.useState(0);
   const [gameOver, setGameOver] = useState(null);
+  const [modal, setModal] = useState(false);
+  const [name, setName] = useState("");
+  const submitScore = () => {
+    //timeout for animation
+    setTimeout(() => {
+      //code here
+    }, 500);
+  };
+
   React.useEffect(() => {
     const int = setInterval(() => {
       const scoreEl = document.querySelector(".running-score");
@@ -22,12 +38,58 @@ function Contest() {
   }, []);
   React.useEffect(() => {
     if (gameOver) {
-      console.log("GameOver");
+      setModal(true);
     }
   }, [gameOver]);
 
   return (
     <div>
+      {modal && (
+        <Modal>
+          <ElevatedCard
+            edgeColors={{
+              bottom: colorPalette.popWhite[100],
+              right: colorPalette.popWhite[300],
+            }}
+            style={{
+              width: "300px",
+            }}
+          >
+            <div className="flex flex-col w-full h-full  items-center border-t-[1px] border-l-[1px] py-3 bg-[#0d0d0d] px-3">
+              <Typography
+                {...FontVariant.HeadingBold28}
+                color={colorPalette.white}
+                fontSize={24}
+                style={{ margin: " 0.25rem 0.2rem 1rem 0.2rem " }}
+              >
+                Your score is {score}
+              </Typography>
+              <InputField
+                type="text"
+                label="your name"
+                placeholder="enter your name"
+                id="text_field"
+                onChange={(e) => setName(e.target.value)}
+                autoFocus
+              />
+              <Button
+                variant="secondary"
+                kind="elevated"
+                size="medium"
+                colorMode="light"
+                fullWidth
+                style={{ margin: " 1rem 0.2rem 0.25rem 0.2rem " }}
+                onClick={() => {
+                  submitScore();
+                }}
+                showArrow
+              >
+                Submit Entry
+              </Button>
+            </div>
+          </ElevatedCard>
+        </Modal>
+      )}
       <div className="w-fit mx-auto">
         <ElevatedCard
           backgroundColor="transparent"
